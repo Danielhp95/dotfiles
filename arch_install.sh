71629713567bd6871584be8a46d55fcc4d99e508
Sarios <danielhp95@gmail.com>
2020-02-18 10:02:36 +0000

    Added bootstraps installation scripts for arch and debian

---
+++ arch_install.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+
+function install_yay() {
+    pacman -S yay
+}
+
+function install_xclip() {
+    yay -S xclip
+}
+
+function install_firefox() {
+    yay -S firefox
+}
+
+function install_tmux() {
+    yay -S tmux
+}
+
+function install_ripgrep() {
+    yay -S ripgrep
+}
+
+function install_oh_my_zsh() {
+    # Install zsh
+    yay -S zsh
+    # Installs oh_my_zsh
+    curl -L http://install.ohmyz.sh | sh
+    # Makes zsh default terminal
+    chsh -s $(which zsh)
+}
+
+function install_telegram() {
+    yay -S telegram-desktop-bin
+}
+
+function install_xcape() {
+    yay -s xcape-git
+}
+
+function install_wpgtk() {
+    yay -S wpgtk-git
+}
+
+function install_bat() {
+    yay -S bat
+}
+
+function install_exa() {
+    yay -S exa
+}
+
+function install_i3status_rust() {
+    yay -S i3status-rust-git
+}
+
+
+function install_rofi() {
+    yay -S rofi
+    yay -S rofi-dmenu
+}
+
+function install_mpv() {
+    yay -S mpv
+}
+
+# Power (battery) management
+function install_tlp() {
+    yay -S tlp
+}
+
+function install_zathura() {
+    yay -S zathura
+    yay -S zathura-pdf-mupdf
+}
+
+function install_pip() {
+   yay -S python-pipenv
+}
+
+function install_ipython() {
+   yay -S ipython
+}
+
+# Big package. 1.5GB
+function install_latex() {
+   yay -S texlive-most
+}
+
+# Syntax checker for python
+function install_flake8() {
+   sudo pip install flake8
+}
+
+function install_kitty_terminal() {
+   yay -S kitty
+}
+
+function install_silver_searcher() {
+   yay -S the_silver_searcher
+}
+
+function install_universal_ctags() {
+   yay -S universal-ctags-git
+}
+
+function install_cmake() {
+   yay -S cmake
+}
+
+function install_gotop() {
+    # System monitoring tool
+    yay -S gotop
+}
+
+function install_vim() {
+    copy_vimrc_file
+    install_vim_plugins
+}
+
+function copy_vimrc_file() {
+    echo "TODO"
+}
+
+function install_vim_plugins() {
+    vim +PluginUpdate +qall
+}
+
+function install_i3_alternating_layouts() {
+    # NOT WORKING
+    sudo pip install i3-py
+    git clone https://github.com/olemartinorg/i3-alternating-layout ~/.config/i3/scripts/
+}
+
+
+function install_coc_vim_python() {
+    pip install python-language-server
+    pip install 'python-language-server[rope]'
+}
+
+
+#########
+# Fonts #
+#########
+function install_fira_code_font() {
+    yay -S otf-fira-code
+}
+
+function install_font_awesome() {
+    yay -S ttf-font-awesome-4
+}
+
+#####################
+# Images / Pictures #
+#####################
+function install_feh() {
+    yay -S feh
+}
+
+function install_maim() {
+    yay -S maim
+}
+
+function install_compton_tryone() {
+    yay -S compton-tryone-git
+}
+
+function install_omnipause() {
+    yay -S python2-dbus
+    git clone https://github.com/mel00010/OmniPause
+    cd OmniPause/
+    sudo make install
+    cd ..
+    rm -rf OmniPause/
+}
+
+function install_entr() {
+    yay -S entr
+}
+
+function install_jekyll() {
+    yay -S rubygems
+    gem update
+    gem install jekyll
+    gem install bundler
+    bundle update --bundler
+    # remember that it is needed to extend $PATH
+    # PATH="$PATH:$(ruby -e 'puts Gem.user_dir')/bin"
+}
+
+function install_all() {
+    install_yay
+    install_oh_my_zsh
+    install_rofi
+    install_tmux
+    install_i3status_rust
+
+    # Fonts
+    install_fira_code_font
+    install_font_awesome
+
+    # Aesthetics
+    install_wpgtk
+    install_compton_tryone
+
+    # Small utilities
+    install_bat
+    install_exa
+    install_feh
+    install_xcape
+    install_xclip
+    install_silver_searcher
+    install_cmake
+    install_universal_ctags
+    install_maim
+    install_omnipause
+    install_entr
+    install_gotop
+
+    # VIM
+    install_vim
+
+    # Programming languages
+    install_pip
+    install_ipython
+    install_flake8
+
+    install_latex
+
+    # Personal usage
+    install_firefox
+    install_telegram
+    install_mpv
+    install_tlp
+    install_zathura
+    install_kitty
+    install_jekyll
+    install_ripgrep
+
+    # Change shell to zsh
+    chsh -s /usr/bin/zsh
+}
+
+install_all
---
+++ debian_install.sh
@@ -0,0 +1,260 @@
+#!/bin/bash
+
+function head_install_mpv() {
+    sudo add-apt-repository ppa:mc3man/mpv-tests
+    sudo apt update
+    sudo apt install mpv
+}
+
+function head_install_spotify() {
+    # 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+
+    # 2. Add the Spotify repository
+    echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+    # 3. Update list of available packages
+    sudo apt-get update
+
+    # 4. Install Spotify
+    sudo apt-get install spotify-client
+}
+
+function install_pip() {
+    sudo apt-get install python-pip
+    sudo apt-get install python3-pip
+    sudo pip install setuptools
+}
+
+function install_ipython() {
+    sudo apt-get install ipython
+    sudo apt-get install ipython3
+}
+
+# Syntax checker for python
+function install_flake8() {
+    sudo python -m pip install flake8
+    sudo python3 -m pip install flake8
+}
+
+function install_pipenv() {
+    sudo pip3 install pew
+    sudo pip install pipenv
+}
+
+function install_silver_searcher() {
+    sudo apt-get install silversearcher-ag
+}
+
+function install_ctags() {
+    sudo apt install exuberant-ctags
+}
+
+function install_tree() {
+    sudo apt-get install tree
+}
+
+# Will need to change when new version arrives
+# Isn't working anymore?
+function install_python36() {
+    sudo add-apt-repository ppa:jonathonf/python-3.6
+    sudo apt update
+    sudo apt install python3.6
+    sudo apt install python3.6-dev
+    wget https://bootstrap.pypa.io/get-pip.py
+    sudo python3.6 get-pip.py
+    rm get-pip.py
+}
+
+function install_openssh_server() {
+    sudo apt-get install openssh-server
+}
+
+function install_latex_compiler() {
+    sudo apt-get install texlive-latex-base
+    sudo apt-get install lacheck
+    sudo apt-get install latex-beamer
+    sudo apt-get install texlive-full # This package is +3GB big. Use apt-get install texlive for lightweight version
+}
+
+function install_zathura() {
+    sudo apt-get install zathura
+}
+
+function install_vim() {
+    sudo apt-get install vim
+    install_vundle
+    install_you_complete_me
+    install_ctags
+}
+
+# Remember that it is necessary to change .vimrc
+function install_vundle() {
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+}
+
+# The --all flag will probably fail due to dependencies. Fix as appropiate
+function install_you_complete_me() {
+    sudo apt-get install build-essential cmake
+    sudo apt-get install python-dev python3-dev
+    cd ~/.vim/bundle/YouCompleteMe
+    ./install.py --all
+    cd -
+}
+
+function install_fzf() {
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
+
+function install_xclip() {
+    sudo apt-get install xclip -y
+}
+
+function install_unzip() {
+    sudo apt-get install unzip -y
+}
+
+function install_mosh() {
+    sudo apt-get install mosh -y
+}
+
+# Installs both JDK and JRE
+function install_java10() {
+    echo oracle-java10-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+    sudo add-apt-repository ppa:linuxuprising/java
+    sudo apt update
+    sudo apt install oracle-java10-installer
+    oracle-java10-set-default package
+}
+
+function head_install_telegram() {
+    sudo add-apt-repository ppa:atareao/telegram
+    sudo apt-get update
+    sudo apt-get install telegram
+}
+
+function install_mono() {
+    sudo apt-get install mono-complete
+}
+
+function install_nuget() {
+    sudo apt install nuget
+}
+
+function install_skype() {
+    sudo apt install apt-transport-https
+    curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+    echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
+    sudo apt update
+    sudo apt install skypeforlinux
+}
+
+# Used mainly for vim airline fonts.
+function install_powerline() {
+    apt-get install python-pip
+    sudo pip install git+git://github.com/Lokaltog/powerline
+    wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+    wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+    sudo mv PowerlineSymbols.otf /usr/share/fonts/
+    # fc-cache builds font information. -v for verbose, -f for forcing even up to date cached builds.
+    sudo fc-cache -vf /usr/share/fonts/
+    sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+}
+
+function install_docker() {
+    # Taken from official page: https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+    sudo apt install docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+}
+
+function install_base_16(){
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+}
+
+function install_tmux() {
+    sudo apt-get install tmux
+    # Install Tmux plugin manager. TPM
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
+function install_git() {
+	sudo apt-get install git
+    # Sets a global gitignore
+    git config --global core.excludesfile '~/.gitignore'
+}
+
+function copy_latest_configs() {
+    git clone https://github.com/Danielhp95/configs.git
+    cd configs/
+    python setup.py
+    cd ..
+    rm -rf configs/
+}
+
+
+function install_flux() {
+    sudo add-apt-repository ppa:nathan-renniewaldock/flux
+    sudo apt-get update
+    sudo apt-get install fluxgui
+}
+
+function install_pandoc() {
+    sudo apt-get install pandoc
+    sudo apt-get install pandoc-citeproc
+}
+
+function install_all() {
+    install_git
+
+    # Programming language tools
+    install_pipenv
+    install_pip	
+
+    # Programming languages
+    install_java10
+    install_ipython
+    install_latex_compiler
+    install_mono # c# linux compiler
+    install_nuget # c# kinda pip
+    install_flake8 # python syntax checker
+    # install_python36  Ubuntu 18 comes with this
+
+    # Media programs
+    head_install_telegram
+    head_install_mpv
+    head_install_spotify
+
+
+    # Shell tools
+    install_openssh_server
+    install_mosh
+    install_fzf
+    install_xclip
+    install_silver_searcher
+    install_unzip
+    install_base_16
+    install_tmux
+    install_powerline
+    install_tree
+
+    # Health
+    install_flux
+
+    # All hail
+    install_vim
+    install_zathura
+    install_docker
+
+    # Livestyle utilities
+    install_pandoc
+
+    # Config files
+    copy_latest_configs
+
+    # Untrusted key on repo. Won't work unless fixed
+    # install_skype
+}
+
+install_all
