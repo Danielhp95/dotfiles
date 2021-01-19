266e837914e6a886116510534737977dbbbffe7e
daniel <danielhp95@gmail.com>
2021-01-19 22:03:19 +0100

    Updated script so that it doesn't call zathura if no paper is chosen

--- rofi/scripts/open_paper.sh
+++ rofi/scripts/open_paper.sh
@@ -1,1 +1,4 @@
-zathura ~/papers/$(ls ~/papers/ | rofi -dmenu)
+PAPERS_LOCATION=~/papers
+PAPER=$(ls $PAPERS_LOCATION | rofi -dmenu)
+# $? checks the return code of the last program
+(($? == 0 )) && zathura $PAPERS_LOCATION/$PAPER
