82df62e1e12a7886e41b9d96e0a7b7eef18fa408
daniel <danielhp95@gmail.com>
2021-01-27 00:21:16 +0100

    Refactor tmux.conf and bluetoothctl script now reconnects currenly connected devices if chosen

--- i3/scripts/choose_bluetooth_device_from_paired.sh
+++ i3/scripts/choose_bluetooth_device_from_paired.sh
@@ -1,7 +1,10 @@
 bluetoothctl power on
 bluetoothctl agent on
-bluetoothctl devices | rofi -dmenu | awk '{print $2}' | xargs bluetoothctl connect
+device=$(bluetoothctl devices | rofi -dmenu | awk '{print $2}') 
 if [ $? -eq 0 ]; then
+    # This allows reconnection to a device
+    bluetoothctl disconnect
+    bluetoothctl connect $device
     rofi -e "Great Success!"
 else
     rofi -e "Failed to connect"
